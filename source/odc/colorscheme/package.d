178ddfb5aecd89e6eff4d20a96ca73e14304f856
crazymonkyyy <crazymonkyyy@gmail.com>
2025-01-13 22:21:43 -0700

    whyyyyy didnt you just commit; YOU BAKA

--- source/odc/colorscheme/package.d
+++ source/odc/colorscheme/package.d
@@ -1,10 +1,47 @@
-import odc.templatehell.meta;
+import odc.templatehell;
 import odc.raylib.raylib_types;
 
 enum string colorcsv()=import("base-16.csv");
 alias colors=Color[16];
-enum colors[string]colorschemes=(){
+enum colors[string] colorschemes=(){
 	colors[string] o;
-	
+	foreach(row;colorcsv!().range.split!(a=>a=='\n')){
+		auto row_=row.split!(a=>a==',');
+		string s=row_.front.array;
+		colors cs;
+		int i=0;
+		row_.popFront;
+		foreach(c;row_){
+			cs[i++]=Color(c.array);
+		}
+		o[s]=cs;
+	}
 	return o;
 }();
+R dup(R)(R r)=>r;
+auto split(alias F,R)(R r){
+	struct Split{
+		R r;
+		auto front()=>r.dup.takeuntil!(a=>F(a.front));
+		void popFront(){r=r.find!(a=>F(a)).find!(a=> ! F(a));}
+		bool empty()=>r.empty;
+	}
+	return Split(r.find!(a=> ! F(a)));
+}
+unittest{
+	//colorcsv!().range.split!(a=>a=='\n').front.split!(a=>a==',').summery;
+	//counter(100).split!(a=>a%10==0).summery;
+}
+unittest{
+	//colorschemes.summery;
+}
+auto array(R)(R r){
+	typeof(r.front())[] o;//TODO:
+	foreach(e;r){
+		o~=e;
+	}
+	return o;
+}
+alias currentcolorscheme_()=innate!(string,"solarized-dark","color123");
+void currentcolorscheme()(string s){currentcolorscheme_=s;}
+auto currentcolorscheme()()=>colorschemes[currentcolorscheme_!()];
--- source/test2.d
+++ source/test2.d
@@ -2,7 +2,7 @@
 import odc.templatehell;
 import odc.raylib;
 import odc.colorscheme;
-maxlengtharray!(Rectangle,8) data;
+maxlengtharray!(Rectangle,16) data;
 
 void main(){
 	InitWindow(800, 640, "raylib");
@@ -12,8 +12,8 @@ void main(){
 	while (!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(BLACK);
-		foreach(r;data[]){
-			DrawRectangleRec(r,RED);
+		foreach(r;zip(data[],currentcolorscheme.range)){
+			DrawRectangleRec(r.expand);
 		}
 		if(IsMouseButtonPressed(0)){
 			data~=Rectangle(GetMouseX,GetMouseY,30,30);
